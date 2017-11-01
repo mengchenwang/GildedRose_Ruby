52d21e7af7e030ec7207a354ec7f62710a222f76
mengchenwang <mengchenwang@hotmail.com>
2017-11-01 17:35:21 +0000

    created router and Sulfuras class

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'simplecov', :require => false, :group => :test
 gem 'rspec'
+gem 'flog', '~> 4.3', '>= 4.3.2'
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,8 @@
+require_relative 'item'
+
 class GildedRose
+
+  SPECIAL_ITEMS = ["Aged Brie", "Sulfuras", "Backstage pass", "Conjured"]
 
   def initialize(items)
     @items = items
@@ -6,63 +10,20 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      special_item?(item) ? update_special_items(item) : Normal_item.update(item)
     end
   end
-end
 
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
+  def special_item?(item)
+    SPECIAL_ITEMS.any? { |short_name| item.name.include?(short_name) }
   end
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+  def update_special_items(item)
+    SPECIAL_ITEMS.each do |short_name|
+      if item.name.include?(short_name)
+        item_class_name = short_name.tr(' ', '_')
+        Object.const_get(item_class_name).update(item)
+      end
+    end
   end
 end
